56c54ace9dc20c8b940f63dedd4e32b1cdcfb329
李祥捷 <773983534@qq.com>
2023-02-19 14:56:38 +0800

    init

---
+++ src/main/java/com/lxj/thymyleafDemo/ThymyleafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lxj.thymyleafDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymyleafDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymyleafDemoApplication.class, args);
+	}
+
+}
